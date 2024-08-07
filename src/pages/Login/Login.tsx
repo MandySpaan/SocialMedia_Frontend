4bbdf7e8a9d4d8ff45da97579652416592ee0be7
MandySpaan <mandyspaan94@gmail.com>
2024-08-07 16:19:41 +0200

    feat(Login): css added for Login page

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -6,20 +6,20 @@ const Login = () => {
       <div className="intro-container">
         <h1>GeekTok</h1>
         <p>
-          Share Your Love for All Things Geek and <br />
-          Connect with Fellow Geeks Worldwide
+          Share Your Love for All Things Geek and Connect with Fellow Geeks
+          Worldwide
         </p>
-        <div className="login-container">
-          <input type="text" name="email" id="email" placeholder="Email" />
-          <input
-            type="password"
-            name="password"
-            id="password"
-            placeholder="Password"
-          />
-          <input type="button" value="Login" />
-          <p>No account yet? Register here</p>
-        </div>
+      </div>
+      <div className="login-container">
+        <input type="text" name="email" id="email" placeholder="Email" />
+        <input
+          type="password"
+          name="password"
+          id="password"
+          placeholder="Password"
+        />
+        <input type="button" value="Login" />
+        <p>No account yet? Register here</p>
       </div>
     </div>
   );
