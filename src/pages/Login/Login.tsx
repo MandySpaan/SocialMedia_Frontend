6cc1aee055c7aced41a76da08e404ec82ca0c3bd
MandySpaan <mandyspaan94@gmail.com>
2024-08-07 14:52:49 +0200

    feat(Login): created intro container

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,5 +1,17 @@
+import "./Login.css";
+
 const Login = () => {
-  return <div>Login</div>;
+  return (
+    <div className="login-page">
+      <div className="intro-container">
+        <h1>GeekTok</h1>
+        <p>
+          Share Your Love for All Things Geek and <br />
+          Connect with Fellow Geeks Worldwide
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default Login;
