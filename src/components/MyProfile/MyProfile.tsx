ee05ad00fc7aad34dd4ff435e5b8d53ce1e414c9
MandySpaan <mandyspaan94@gmail.com>
2024-08-09 09:45:29 +0200

    feat(MyProfile): added description and organized layout

--- src/components/MyProfile/MyProfile.tsx
+++ src/components/MyProfile/MyProfile.tsx
@@ -6,10 +6,13 @@ import "./MyProfile.css";
 const MyProfile = () => {
   const [profileData, setProfileData] = useState({
     username: "",
+    description: "",
     first_name: "",
     last_name: "",
     email: "",
   });
+
+  const [editing, setEditing] = useState(false);
 
   const { passport } = useAuth();
 
@@ -21,12 +24,36 @@ const MyProfile = () => {
     bringMyProfile();
   }, []);
 
+  const editview = () => {
+    setEditing(!editing);
+  };
+
+  console.log(editing);
+
   return (
     <div className="myprofile-box">
       <h2>{profileData.username}</h2>
-      <p>
-        {profileData.first_name} {profileData.last_name}
-      </p>
+      <div
+        className={
+          !profileData.first_name && !profileData.last_name
+            ? "only-editlink"
+            : "fullname-editlink"
+        }
+      >
+        <div className="full-name bold">
+          <p>
+            {profileData.first_name} {profileData.last_name}
+          </p>
+        </div>
+        <div className="link-to-edit">
+          <p>
+            <span onClick={editview} className="link-style">
+              Edit your profile
+            </span>
+          </p>
+        </div>
+      </div>
+      <p>{profileData.description}</p>
     </div>
   );
 };
