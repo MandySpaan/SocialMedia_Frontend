17a072b79ab6439f9424dbe235d654750beac1dd
MandySpaan <mandyspaan94@gmail.com>
2024-08-10 20:47:42 +0200

    feat(AllPosts): set up the layout for the AllPost page

--- src/pages/AllPosts/AllPosts.tsx
+++ src/pages/AllPosts/AllPosts.tsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import { getAllPosts } from "../../services/postsApiCalls";
 import Navbar from "../../components/Navbar/Navbar";
+import "./AllPosts.css";
 
 const AllPosts = () => {
   const [posts, setPosts] = useState([]);
@@ -21,29 +22,33 @@ const AllPosts = () => {
   return (
     <>
       <Navbar />
-      <div className="about-page">
+      <>
         <h1>All Posts</h1>
         {posts.length === 0 ? (
           <p>No posts found...</p>
         ) : (
-          <div className="services-collection">
+          <div className="allposts-page">
             {posts.map(
               (post: {
                 _id: string;
+                user_id: { username: string };
                 title: string;
                 description: string;
-                likes: number;
+                likes: string[];
               }) => (
                 <div className="allposts-container" key={post._id}>
-                  <div className="title bold">{post.title}</div>
-                  <div className="likes">{post.likes}</div>
+                  <div className="username">{post.user_id.username}</div>
+                  <div className="title-likes">
+                    <div className="title bold">{post.title}</div>
+                    <div className="likes">likes: {post.likes.length}</div>
+                  </div>
                   <div className="description">{post.description}</div>
                 </div>
               )
             )}
           </div>
         )}
-      </div>
+      </>
     </>
   );
 };
