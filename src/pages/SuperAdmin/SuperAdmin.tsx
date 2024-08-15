ced5b0a31829a7b92e52ebab826eee22b28710b7
MandySpaan <mandyspaan94@gmail.com>
2024-08-15 11:09:30 +0200

    feat(SuperAdmin): added all posts to SuperAdmin page

--- src/pages/SuperAdmin/SuperAdmin.tsx
+++ src/pages/SuperAdmin/SuperAdmin.tsx
@@ -3,9 +3,11 @@ import "./SuperAdmin.css";
 import { useAuth } from "../../contexts/AuthContext";
 import { useEffect, useState } from "react";
 import { getAllUsers } from "../../services/userApiCalls";
+import { getAllPosts } from "../../services/postsApiCalls";
 
 const SuperAdmin = () => {
   const [users, setUsers] = useState([]);
+  const [posts, setPosts] = useState([]);
 
   const { passport } = useAuth();
   const navigate = useNavigate();
@@ -27,6 +29,19 @@ const SuperAdmin = () => {
     bringAllUsers();
   }, [passport]);
 
+  useEffect(() => {
+    const bringAllPosts = async () => {
+      try {
+        const response = await getAllPosts();
+        setPosts(response.data);
+      } catch (error) {
+        console.error("Error fetching services:", error);
+      }
+    };
+
+    bringAllPosts();
+  }, []);
+
   return (
     <div className="superadmin-page">
       <h1>Welcome Super Admin</h1>
@@ -40,18 +55,49 @@ const SuperAdmin = () => {
         geektok.com/super-admin <br /> or get automatically forwarded by simply
         logging out and logging in again
       </div>
-      <div className="all-users">
+      <div className="sa-allusers">
         <h2>All Users</h2>
         {users.length === 0 ? (
           <p>No users found...</p>
         ) : (
-          <div className="all-users-container">
+          <div className="sa-allusers-container">
             {users.map((user: { _id: string; username: string }) => (
               <div className="all-users-card" key={user._id}>
                 <div className="username">{user.username}</div>
                 <button>Delete User</button>
               </div>
             ))}
+          </div>
+        )}
+      </div>
+      <div className="sa-allposts">
+        <h2>All Posts</h2>
+        {posts.length === 0 ? (
+          <p>No posts found...</p>
+        ) : (
+          <div className="sa-allposts-container">
+            {posts.map(
+              (post: {
+                _id: string;
+                user_id: { username: string };
+                title: string;
+              }) => (
+                <div className="all-posts-card" key={post._id}>
+                  <div className="username-title">
+                    <div className="username">{post.user_id.username}</div>
+                    <div
+                      className="title bold"
+                      onClick={() => {
+                        navigate(`/post-details/${post._id}`);
+                      }}
+                    >
+                      {post.title}
+                    </div>
+                  </div>
+                  <button>Delete Post</button>
+                </div>
+              )
+            )}
           </div>
         )}
       </div>
