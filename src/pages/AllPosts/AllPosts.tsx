94cd89c38f129b8e4b9ac014fca7f13c5b2986f0
MandySpaan <mandyspaan94@gmail.com>
2024-08-28 16:35:28 +0200

    feat: made Follow link turn into Unfollow when present in tokenData.following

--- src/pages/AllPosts/AllPosts.tsx
+++ src/pages/AllPosts/AllPosts.tsx
@@ -9,9 +9,8 @@ import "./AllPosts.css";
 
 const AllPosts = () => {
   const [posts, setPosts] = useState([]);
-
+  const [following, setFollowing] = useState<string[]>([]);
   const navigate = useNavigate();
-
   const { passport } = useAuth();
 
   useEffect(() => {
@@ -27,12 +26,26 @@ const AllPosts = () => {
     bringAllPosts();
   }, []);
 
-  const onFollowUser = (
+  useEffect(() => {
+    if (passport?.tokenData?.following) {
+      setFollowing(passport.tokenData.following);
+    }
+  }, [passport]);
+
+  const onFollowUser = async (
     token: string,
     userId: string,
     currentFollowing: string[]
   ) => {
-    handleFollowUser(token, userId, currentFollowing, setPosts);
+    await handleFollowUser(token, userId, currentFollowing, setPosts);
+
+    if (currentFollowing.includes(userId)) {
+      setFollowing((prevFollowing) =>
+        prevFollowing.filter((id) => id !== userId)
+      );
+    } else {
+      setFollowing((prevFollowing) => [...prevFollowing, userId]);
+    }
   };
 
   const onLikePost = (
@@ -59,46 +72,63 @@ const AllPosts = () => {
                 title: string;
                 description: string;
                 likes: string[];
-              }) => (
-                <div className="allposts-container" key={post._id}>
-                  <div className="username-title-likes">
-                    <div className="username-title">
-                      <div className="username-follow">
-                        <div className="username">{post.user_id.username}</div>
+              }) => {
+                const isFollowing = following.includes(post.user_id._id);
+                return (
+                  <div className="allposts-container" key={post._id}>
+                    <div className="username-title-likes">
+                      <div className="username-title">
+                        <div className="username-follow">
+                          <div className="username">
+                            {post.user_id.username}
+                          </div>
+                          <div
+                            className={isFollowing ? "unfollow" : "hidden"}
+                            onClick={() =>
+                              onFollowUser(
+                                passport!.token,
+                                post.user_id._id,
+                                following
+                              )
+                            }
+                          >
+                            {"Unfollow"}
+                          </div>
+                          <div
+                            className={isFollowing ? "hidden" : "follow"}
+                            onClick={() =>
+                              onFollowUser(
+                                passport!.token,
+                                post.user_id._id,
+                                following
+                              )
+                            }
+                          >
+                            {"Follow"}
+                          </div>
+                        </div>
                         <div
-                          className="follow"
-                          onClick={() =>
-                            onFollowUser(
-                              passport!.token,
-                              post.user_id._id,
-                              passport!.tokenData.following
-                            )
-                          }
+                          className="title bold"
+                          onClick={() => {
+                            navigate(`/post-details/${post._id}`);
+                          }}
                         >
-                          {"Follow"}
+                          {post.title}
                         </div>
                       </div>
                       <div
-                        className="title bold"
-                        onClick={() => {
-                          navigate(`/post-details/${post._id}`);
-                        }}
+                        className="likes"
+                        onClick={() =>
+                          onLikePost(passport!.token, post._id, post.likes)
+                        }
                       >
-                        {post.title}
+                        likes: {post.likes.length}
                       </div>
                     </div>
-                    <div
-                      className="likes"
-                      onClick={() =>
-                        onLikePost(passport!.token, post._id, post.likes)
-                      }
-                    >
-                      likes: {post.likes.length}
-                    </div>
+                    <div className="description">{post.description}</div>
                   </div>
-                  <div className="description">{post.description}</div>
-                </div>
-              )
+                );
+              }
             )}
           </div>
         )}
